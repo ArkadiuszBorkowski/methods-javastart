1bd35aae8e4fa9930d0c9c9d3fee7bb49a4b1f6d
ArkadiuszBorkowski <borkowskiarkadiusz91@gmail.com>
2022-08-14 19:41:53 +0200

    Methods and constructors in java

---
+++ src/Calc.java
@@ -0,0 +1,11 @@
+class Calc {
+    double add(double a, double b) {
+        double result = a + b;
+        return  result;
+    }
+
+    void addAndPrint(double a, double b) {
+        double result = a + b;
+        System.out.println(result);
+    }
+}
---
+++ src/CalcTest.java
@@ -0,0 +1,7 @@
+class CalcTest {
+    public static void main(String[] args) {
+        Calc calc = new Calc();
+        double result = calc.add(5,7);
+        System.out.println(result);
+    }
+}
